cf458a4d04f036626f9e1c038d58def0aa84fd22
Ruslan Forostianov <ruslan@se4.bio>
2024-05-07 11:55:01 +0200

    Extract common code from inc. tab. delim. tests

--- src/test/java/org/mskcc/cbio/portal/integrationTest/incremental/TestIncrementalTabDelimData.java
+++ src/test/java/org/mskcc/cbio/portal/integrationTest/incremental/TestIncrementalTabDelimData.java
@@ -17,6 +17,7 @@
 
 package org.mskcc.cbio.portal.integrationTest.incremental;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,10 +26,7 @@ import org.mskcc.cbio.portal.dao.DaoException;
 import org.mskcc.cbio.portal.dao.DaoGeneOptimized;
 import org.mskcc.cbio.portal.dao.DaoGeneticAlteration;
 import org.mskcc.cbio.portal.dao.DaoGeneticProfile;
-import org.mskcc.cbio.portal.dao.DaoSample;
-import org.mskcc.cbio.portal.model.CancerStudy;
 import org.mskcc.cbio.portal.model.GeneticProfile;
-import org.mskcc.cbio.portal.model.Sample;
 import org.mskcc.cbio.portal.scripts.ImportTabDelimData;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
@@ -38,7 +36,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -92,12 +89,7 @@ public class TestIncrementalTabDelimData {
         GeneticProfile mrnaProfile = DaoGeneticProfile.getGeneticProfileByStableId("study_tcga_pub_mrna");
         assertNotNull(mrnaProfile);
         HashMap<Long, HashMap<Integer, String>> beforeResult = DaoGeneticAlteration.getInstance().getGeneticAlterationMap(mrnaProfile.getGeneticProfileId(), null);
-        assertEquals("All but new entrez gene id expected to be in the database for this profile before the upload", TEST_ENTREZ_GENE_IDS.size() - 1, beforeResult.size());
-        assertFalse("No new entrez gene id expected to be in the database for this profile before the upload", beforeResult.containsKey(NEW_GENE_ENTREZ_ID));
-        beforeResult.forEach((entrezGeneId, sampleIdToValue) -> {
-            assertEquals("All but new sample id expected to be in the database for this profile for gene with entrez id " + entrezGeneId + " before the upload", TEST_SAMPLE_IDS.size() - 1, beforeResult.get(entrezGeneId).size());
-            assertFalse("No new entrez gene id expected to be in the database for this profile for gene with entrez id " + entrezGeneId + " before the upload", beforeResult.get(entrezGeneId).containsKey(NEW_SAMPLE_ID));
-        });
+        assertPriorDataState(beforeResult);
 
         File dataFolder = new File("src/test/resources/incremental/tab_delim_data/");
         File dataFile = new File(dataFolder, "data_expression_Zscores.txt");
@@ -116,20 +108,7 @@ public class TestIncrementalTabDelimData {
          * After test assertions
          */
         HashMap<Long, HashMap<Integer, String>> afterResult = DaoGeneticAlteration.getInstance().getGeneticAlterationMap(mrnaProfile.getGeneticProfileId(), null);
-        assertEquals("These genes expected to be found after upload", TEST_ENTREZ_GENE_IDS, afterResult.keySet());
-        afterResult.forEach((entrezGeneId, sampleIdToValue) -> {
-            assertEquals("These sample ids expected to be found for gene with entrez id " + entrezGeneId+ " after upload", TEST_SAMPLE_IDS, afterResult.get(entrezGeneId).keySet());
-            if (entrezGeneId == NEW_GENE_ENTREZ_ID || entrezGeneId == ABSENT_GENE_ENTREZ_ID) {
-                return;
-            }
-            sampleIdToValue.forEach((sampleId, value) -> {
-                if (sampleId == NEW_SAMPLE_ID || sampleId == UPDATED_SAMPLE_ID) {
-                    return;
-                }
-                assertEquals("The associated value is not expected change associated sample id " + sampleId + " and entrez gene id " + entrezGeneId,
-                    beforeResult.get(entrezGeneId).get(sampleId), afterResult.get(entrezGeneId).get(sampleId));
-            });
-        });
+        assertNoChange(beforeResult, afterResult);
         assertEquals("-0.1735", afterResult.get(NEW_GENE_ENTREZ_ID).get(NEW_SAMPLE_ID));
         assertEquals("-0.6412", afterResult.get(NEW_GENE_ENTREZ_ID).get(UPDATED_SAMPLE_ID));
         assertEquals("", afterResult.get(ABSENT_GENE_ENTREZ_ID).get(NEW_SAMPLE_ID));
@@ -147,12 +126,7 @@ public class TestIncrementalTabDelimData {
         GeneticProfile rppaProfile = DaoGeneticProfile.getGeneticProfileByStableId("study_tcga_pub_rppa");
         assertNotNull(rppaProfile);
         HashMap<Long, HashMap<Integer, String>> beforeResult = DaoGeneticAlteration.getInstance().getGeneticAlterationMap(rppaProfile.getGeneticProfileId(), null);
-        assertEquals("All but new entrez gene id expected to be in the database for this profile before the upload", TEST_ENTREZ_GENE_IDS.size() - 1, beforeResult.size());
-        assertFalse("No new entrez gene id expected to be in the database for this profile before the upload", beforeResult.containsKey(NEW_GENE_ENTREZ_ID));
-        beforeResult.forEach((entrezGeneId, sampleIdToValue) -> {
-            assertEquals("All but new sample id expected to be in the database for this profile for gene with entrez id " + entrezGeneId + " before the upload", TEST_SAMPLE_IDS.size() - 1, beforeResult.get(entrezGeneId).size());
-            assertFalse("No new entrez gene id expected to be in the database for this profile for gene with entrez id " + entrezGeneId + " before the upload", beforeResult.get(entrezGeneId).containsKey(NEW_SAMPLE_ID));
-        });
+        assertPriorDataState(beforeResult);
 
         File dataFolder = new File("src/test/resources/incremental/tab_delim_data/");
         File dataFile = new File(dataFolder, "data_rppa.txt");
@@ -171,6 +145,23 @@ public class TestIncrementalTabDelimData {
          * After test assertions
          */
         HashMap<Long, HashMap<Integer, String>> afterResult = DaoGeneticAlteration.getInstance().getGeneticAlterationMap(rppaProfile.getGeneticProfileId(), null);
+        assertNoChange(beforeResult, afterResult);
+        assertEquals("-0.141047088398489", afterResult.get(NEW_GENE_ENTREZ_ID).get(NEW_SAMPLE_ID));
+        assertEquals("1.61253243564957", afterResult.get(NEW_GENE_ENTREZ_ID).get(UPDATED_SAMPLE_ID));
+        assertEquals("", afterResult.get(ABSENT_GENE_ENTREZ_ID).get(NEW_SAMPLE_ID));
+        assertEquals("-1.129", afterResult.get(ABSENT_GENE_ENTREZ_ID).get(UPDATED_SAMPLE_ID));
+    }
+
+    private void assertPriorDataState(HashMap<Long, HashMap<Integer, String>> beforeResult) {
+        assertEquals("All but new entrez gene id expected to be in the database for this profile before the upload", TEST_ENTREZ_GENE_IDS.size() - 1, beforeResult.size());
+        assertFalse("No new entrez gene id expected to be in the database for this profile before the upload", beforeResult.containsKey(NEW_GENE_ENTREZ_ID));
+        beforeResult.forEach((entrezGeneId, sampleIdToValue) -> {
+            assertEquals("All but new sample id expected to be in the database for this profile for gene with entrez id " + entrezGeneId + " before the upload", TEST_SAMPLE_IDS.size() - 1, beforeResult.get(entrezGeneId).size());
+            assertFalse("No new entrez gene id expected to be in the database for this profile for gene with entrez id " + entrezGeneId + " before the upload", beforeResult.get(entrezGeneId).containsKey(NEW_SAMPLE_ID));
+        });
+    }
+
+    private void assertNoChange(HashMap<Long, HashMap<Integer, String>> beforeResult, HashMap<Long, HashMap<Integer, String>> afterResult) {
         assertEquals("These genes expected to be found after upload", TEST_ENTREZ_GENE_IDS, afterResult.keySet());
         afterResult.forEach((entrezGeneId, sampleIdToValue) -> {
             assertEquals("These sample ids expected to be found for gene with entrez id " + entrezGeneId+ " after upload", TEST_SAMPLE_IDS, afterResult.get(entrezGeneId).keySet());
@@ -185,10 +176,6 @@ public class TestIncrementalTabDelimData {
                         beforeResult.get(entrezGeneId).get(sampleId), afterResult.get(entrezGeneId).get(sampleId));
             });
         });
-        assertEquals("-0.141047088398489", afterResult.get(NEW_GENE_ENTREZ_ID).get(NEW_SAMPLE_ID));
-        assertEquals("1.61253243564957", afterResult.get(NEW_GENE_ENTREZ_ID).get(UPDATED_SAMPLE_ID));
-        assertEquals("", afterResult.get(ABSENT_GENE_ENTREZ_ID).get(NEW_SAMPLE_ID));
-        assertEquals("-1.129", afterResult.get(ABSENT_GENE_ENTREZ_ID).get(UPDATED_SAMPLE_ID));
     }
 
 }
