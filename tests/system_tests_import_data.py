65c8b119e4bc3fd02af9689c99852ce3d8d18ee7
Ruslan Forostianov <ruslan@se4.bio>
2024-05-24 08:56:02 +0200

    Add CNA DISCRETE LONG to study_es0_inc test dataset

--- tests/system_tests_import_data.py
+++ tests/system_tests_import_data.py
@@ -109,6 +109,8 @@ class DataImporterTests(unittest.TestCase):
             '--meta', f'{data_directory}/meta_mutations_extended.txt', '--loadMode', 'bulkload', '--update-info', 'False', '--data', f'{data_directory}/data_mutations_extended.maf', '--noprogress')
         cna_discrete_call = call(*common_part, 'org.mskcc.cbio.portal.scripts.ImportProfileData', '--overwrite-existing',
             '--meta', f'{data_directory}/meta_cna_discrete.txt', '--loadMode', 'bulkload', '--update-info', 'False', '--data', f'{data_directory}/data_cna_discrete.txt', '--noprogress')
+        cna_discrete_long_call = call(*common_part, 'org.mskcc.cbio.portal.scripts.ImportProfileData', '--overwrite-existing',
+            '--meta', f'{data_directory}/meta_cna_discrete_long.txt', '--loadMode', 'bulkload', '--update-info', 'False', '--data', f'{data_directory}/data_cna_discrete_long.txt', '--noprogress')
         cna_log2_call = call(*common_part, 'org.mskcc.cbio.portal.scripts.ImportProfileData', '--overwrite-existing',
             '--meta', f'{data_directory}/meta_cna_log2.txt', '--loadMode', 'bulkload', '--update-info', 'False', '--data', f'{data_directory}/data_cna_log2.txt', '--noprogress')
         expression_median_call = call(*common_part, 'org.mskcc.cbio.portal.scripts.ImportProfileData', '--overwrite-existing',
@@ -128,6 +130,7 @@ class DataImporterTests(unittest.TestCase):
             clinical_sample_call,
             mutation_call,
             cna_discrete_call,
+            cna_discrete_long_call,
             cna_log2_call,
             expression_median_call,
             methylation_hm27_call,
