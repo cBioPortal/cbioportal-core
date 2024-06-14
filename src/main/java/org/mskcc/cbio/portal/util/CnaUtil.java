b0a448ea62d53de83ef3c3e163db6c8c9596b55d
Ruslan Forostianov <ruslan@se4.bio>
2024-06-14 16:09:20 +0200

    Remove TODOs that were done

--- src/main/java/org/mskcc/cbio/portal/util/CnaUtil.java
+++ src/main/java/org/mskcc/cbio/portal/util/CnaUtil.java
@@ -45,7 +45,6 @@ public class CnaUtil {
         this.namespaceColumnParser = new NamespaceColumnParser(namespaces, headerParts);
     }
 
-    // TODO inc: update
     public static void storeCnaEvents(
         Set<CnaEvent.Event> existingCnaEvents,
         List<CnaEvent> cnaEventsToAdd
@@ -54,7 +53,6 @@ public class CnaUtil {
             if (!CNA.AMP.equals(cnaEvent.getAlteration()) && !CNA.HOMDEL.equals(cnaEvent.getAlteration())) {
                 continue;
             }
-            // TODO Clean cnv event
             // Revert PR https://github.com/cBioPortal/cbioportal-core/pull/1 breaks importer
             Optional<CnaEvent.Event> existingCnaEvent = existingCnaEvents
                     .stream()
