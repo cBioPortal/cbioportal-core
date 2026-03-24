624c05df304fb4f0b5213ae3686f46114b420d14
James Ko <jamesqko@gmail.com>
2026-03-24 13:01:43 -0400

    remove logging code

--- src/main/java/org/mskcc/cbio/portal/dao/DaoClinicalData.java
+++ src/main/java/org/mskcc/cbio/portal/dao/DaoClinicalData.java
@@ -55,9 +55,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Data Access Object for `clinical` table
  *
