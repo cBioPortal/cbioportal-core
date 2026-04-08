940c462530a98d2c153e2ae56b04ece01f2f3b79
James Ko <jamesqko@gmail.com>
2026-04-08 12:48:46 -0400

    add copyright line

--- src/test/java/org/mskcc/cbio/portal/integrationTest/dao/TestDaoCancerStudyDelete.java
+++ src/test/java/org/mskcc/cbio/portal/integrationTest/dao/TestDaoCancerStudyDelete.java
@@ -1,5 +1,6 @@
 /*
  * This file is part of cBioPortal.
+ * Copyright (C) 2026  Memorial Sloan-Kettering Cancer Center.
  *
  * cBioPortal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
