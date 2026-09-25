bcf3524d00e95610252bbe4676a29cccab517ad3
Zhaoyuan (Ryan) Fu <fuzhaoyuan@gmail.com>
2026-09-25 12:30:57 -0400

    Bump db.version to 3.0.1
    - pom.xml: db.version 3.0.1, matching the cbioportal 3.0.1 migration that
      sets cancer_study.status 0 to 1
    - seed_mini.sql: test info.db_schema_version 3.0.1 so DaoInfo.checkVersion
      passes

--- src/test/resources/seed_mini.sql
+++ src/test/resources/seed_mini.sql
@@ -545,7 +545,7 @@ INSERT INTO authorities (`email`, `authority`) values ('Dhorak@yahoo.com','ROLE_
 INSERT INTO authorities (`email`, `authority`) values ('Dhorak@yahoo.com','ROLE_MANAGER');
 
 INSERT INTO `info` (`db_schema_version`, `geneset_version`)
-VALUES ('3.0.0', 'test_geneset_version');
+VALUES ('3.0.1', 'test_geneset_version');
 
 --geneset
 INSERT INTO geneset (`id`, `genetic_entity_id`, `external_id`, `name`, `description`, `ref_link`) VALUES (1, 1, 'HGNC:1100', 'BRCA1', 'Breast cancer type 1 susceptibility protein', 'https://www.ncbi.nlm.nih.gov/gene/672');
